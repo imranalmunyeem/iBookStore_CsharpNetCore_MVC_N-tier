248a82c961b3c8dcb64fa239d64f3133aa2b4d2a
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2023-08-22 01:30:24 +0100

    Added Book model

---
+++ iBookShop.Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iBookShop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+  
+        [MaxLength(13)]
+        public string ISBN { get; set; }
+
+        [Required,
+        DataType(DataType.Date),
+        Display(Name = "Date Published")]
+        public DateTime DatePublished { get; set; }
+
+        [Required,
+        DataType(DataType.Currency)]
+        public int Price { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int SubCategoryId { get; set; }
+
+        public int SubCategory SubCategory {get; set;}
+
+
+}
+}
