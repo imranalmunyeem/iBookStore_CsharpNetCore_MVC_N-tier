405da87942f6c16a48e93d96976be6196c916fc3
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2023-08-22 01:18:32 +0100

    Added SubCategory Model

--- iBookShop.Models/SubCategory.cs
+++ iBookShop.Models/SubCategory.cs
@@ -7,11 +7,16 @@ using System.Threading.Tasks;
 
 namespace iBookShop.Models
 {
-    public class Category
+    public class SubCategory
     {
         [Key]
         public int Id { get; set; }
         [Required]
+
         public string Title { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
