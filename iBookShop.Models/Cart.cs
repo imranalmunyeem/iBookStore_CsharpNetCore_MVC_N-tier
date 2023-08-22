f629a7f0e924b3803fce5c5acdf231cb12b011c3
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2023-08-22 01:46:23 +0100

    Added Cart Model

---
+++ iBookShop.Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iBookShop.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public string BookId { get; set; }
+
+        public Book Book { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required, MinLength(1)]
+
+        public int Count { get; set; }
+
+
+
+    }
+}
